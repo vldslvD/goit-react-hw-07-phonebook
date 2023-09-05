54a5a40e81f3f0e8862a39b31f1e039ccb1217d6
vldslvD <dobritsavlad@gmail.com>
2023-09-05 23:18:15 +0200

    hw7_v.2

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,14 +2,17 @@ import { Form } from 'components/Form/Form';
 import { Filter } from 'components/Filter/Filter';
 import { ContactList } from 'components/ContactList/ContactList';
 import { Container } from 'components/App/App.styled';
+import { contactsSelectors } from 'redux/contacts';
+import { useSelector } from 'react-redux';
 
 export const App = () => {
+    const contacts = useSelector(contactsSelectors.getContacts);
   return (
     <Container>
       <h1>Phonebook</h1>
       <Form />
       <Filter />
-      <h2>Contacts</h2>
+      {contacts.length> 0 && <h2>Contacts</h2>}
       <ContactList />
     </Container>
   );
